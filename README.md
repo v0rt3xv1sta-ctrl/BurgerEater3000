8caa3f5cf062d7e009384b1accf15a9dc7db79ea
v0rt3xv1sta-ctrl <v0rt3xv1sta@gmail.com>
2025-12-11 20:08:17 -0500

    Main

---
+++ README.md
@@ -0,0 +1,12 @@
+# Burger Eater 3000
+
+A simple burger clicker game made in Python with Tkinter.  
+Click "Eat Burger" to earn burgers. Spend 20 burgers to unlock the automatic bot that earns burgers for you!
+
+## How to play
+1. Run `python3 BurgerEater3000.py`
+2. Click "Eat Burger" to earn burgers
+3. When you have 20 burgers, click "Unlock Bot" to start automatic burger collection
+
+## License
+This project is licensed under the GNU General Public License v3.0 (GPL-3.0)
---
+++ main.py
@@ -0,0 +1,53 @@
+# Burger Eater 3000
+# Copyright (C) 2025 Jasper Gulka
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+
+import tkinter as tk
+import time
+import threading
+
+bur = 0
+has_bot = False
+
+window = tk.Tk()
+
+window.title("Burger Eater 3000")
+
+cout = tk.Label(window, text=f"Burger:{bur}")
+cout.pack(pady=20)
+
+def eat():
+    global bur
+    bur += 1
+
+    cout.config(text=f"Burger:{bur}")
+
+def bot_worker():
+    global bur
+    while has_bot:
+        time.sleep(1)
+        bur += 1
+        cout.config(text=f"Burger:{bur}")
+
+def getbot1():
+    global bur, has_bot
+    if bur >= 20:
+        has_bot = True
+        threading.Thread(target=bot_worker, daemon=True).start()
+        bur -= 20
+        cout.config(text=f"Burger:{bur}")
+    else:
+        cout.config(text="you dont have egofe for this")
+
+
+but = tk.Button(window, text="Eat Burger", command=eat)
+but.pack(pady=20)
+
+bot = tk.Button(window, text="unlock bot (20 Burgers)", command=getbot1)
+bot.pack(pady=20)
+
+window.mainloop()
